3354f34c62e15846a269222773146204d6f0496d
Ole Bittner <dev@oleb.it>
2022-12-10 14:25:52 +0100

    Day 10: Cathode-Ray Tube - Part 2

--- src/day10/CathodeRayTube.java
+++ src/day10/CathodeRayTube.java
@@ -9,23 +9,23 @@ public class CathodeRayTube {
     public static void main(String[] args) {
         List<String> input = Util.readInput("day10/CathodeRayTube.txt");
         System.out.println(part1(input));
-        //System.out.println(part2(input));
     }
 
     public static int part1(List<String> input) {
         int cycle = 0;
-        int signalStrenght = 0;
+        int signalStrength = 0;
         int X = 1;
         for (String inst : input) {
             Instruction instruction = parseInstruction(inst);
             do {
                 cycle++;
                 if ((cycle - 20) % 40 == 0)
-                    signalStrenght += X * cycle;
+                    signalStrength += X * cycle;
+                printCRT(X, cycle);
                 X = instruction.execute(X);
             } while (!instruction.isDone());
         }
-        return signalStrenght;
+        return signalStrength;
     }
 
     private static Instruction parseInstruction(String inst) {
@@ -37,6 +37,16 @@ public class CathodeRayTube {
             instruction = new AddInstruction(Integer.parseInt(cmd[1]));
         }
         return instruction;
+    }
+
+    private static void printCRT(int X, int cycle) {
+        int position = (cycle - 1) % 40;
+        if (X - 1 <= position && position <= X + 1)
+            System.out.print('◼');
+        else
+            System.out.print('◻');
+        if (position == 39)
+            System.out.println();
     }
 
 }
