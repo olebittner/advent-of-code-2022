dc9467fdc6f94b83f26064b84f1b2bee6c3d4bf4
Ole Bittner <dev@oleb.it>
2022-12-13 19:24:01 +0100

    Day 11: Monkey in the Middle - optimization

--- src/day11/MonkeyInTheMiddle.java
+++ src/day11/MonkeyInTheMiddle.java
@@ -1,10 +1,9 @@
 package day11;
 
-import java.math.BigInteger;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
-import java.util.function.Function;
+import java.util.function.LongFunction;
 
 public class MonkeyInTheMiddle {
 
@@ -13,13 +12,13 @@ public class MonkeyInTheMiddle {
         System.out.println(part2());
     }
 
-    public static BigInteger part1() {
-        List<Monkey> monkeys = buildMonkeyList(w -> w.divide(BigInteger.valueOf(3)));
+    public static long part1() {
+        List<Monkey> monkeys = buildMonkeyList(w -> (long) (w / 3d));
         letTheMonkeysPlay(monkeys, 20);
         return calcMonkeyBusinessLevel(monkeys);
     }
 
-    public static BigInteger part2() {
+    public static long part2() {
         List<Monkey> monkeys = buildMonkeyList(w -> w);
         letTheMonkeysPlay(monkeys, 10000);
         return calcMonkeyBusinessLevel(monkeys);
@@ -35,23 +34,23 @@ public class MonkeyInTheMiddle {
         }
     }
 
-    static BigInteger calcMonkeyBusinessLevel(List<Monkey> monkeys) {
-        return monkeys.stream().map(Monkey::getOperationCount).sorted(Comparator.reverseOrder()).limit(2).map(BigInteger::valueOf).reduce(BigInteger::multiply).orElse(BigInteger.ZERO);
+    static long calcMonkeyBusinessLevel(List<Monkey> monkeys) {
+        return monkeys.stream().map(Monkey::getOperationCount).sorted(Comparator.reverseOrder()).limit(2).mapToLong(Long::longValue).reduce((left, right) -> left * right).orElse(-1);
     }
 
-    static List<Monkey> buildMonkeyList(Function<BigInteger, BigInteger> worryReduceFunction) {
+    static List<Monkey> buildMonkeyList(LongFunction<Long> worryReduceFunction) {
         List<Monkey> monkeys = List.of(
-                new Monkey(Arrays.asList(98, 89, 52), w -> w.multiply(BigInteger.valueOf(2)), worryReduceFunction, 5, 6, 1),
-                new Monkey(Arrays.asList(57, 95, 80, 92, 57, 78), w -> w.multiply(BigInteger.valueOf(13)), worryReduceFunction, 2, 2, 6),
-                new Monkey(Arrays.asList(82, 74, 97, 75, 51, 92, 83), w -> w.add(BigInteger.valueOf(5)), worryReduceFunction, 19, 7, 5),
-                new Monkey(Arrays.asList(97, 88, 51, 68, 76), w -> w.add(BigInteger.valueOf(6)), worryReduceFunction, 7, 0, 4),
-                new Monkey(Arrays.asList(63), w -> w.add(BigInteger.valueOf(1)), worryReduceFunction, 17, 0, 1),
-                new Monkey(Arrays.asList(94, 91, 51, 63), w -> w.add(BigInteger.valueOf(4)), worryReduceFunction, 13, 4, 3),
-                new Monkey(Arrays.asList(61, 54, 94, 71, 74, 68, 98, 83), w -> w.add(BigInteger.valueOf(2)), worryReduceFunction, 3, 2, 7),
-                new Monkey(Arrays.asList(90, 56), w -> w.multiply(w), worryReduceFunction, 11, 3, 5)
+                new Monkey(Arrays.asList(98L, 89L, 52L), w -> w * 2, worryReduceFunction, 5, 6, 1),
+                new Monkey(Arrays.asList(57L, 95L, 80L, 92L, 57L, 78L), w -> w * 13, worryReduceFunction, 2, 2, 6),
+                new Monkey(Arrays.asList(82L, 74L, 97L, 75L, 51L, 92L, 83L), w -> w + 5, worryReduceFunction, 19, 7, 5),
+                new Monkey(Arrays.asList(97L, 88L, 51L, 68L, 76L), w -> w + 6, worryReduceFunction, 7, 0, 4),
+                new Monkey(Arrays.asList(63L), w -> w + 1, worryReduceFunction, 17, 0, 1),
+                new Monkey(Arrays.asList(94L, 91L, 51L, 63L), w -> w + 4, worryReduceFunction, 13, 4, 3),
+                new Monkey(Arrays.asList(61L, 54L, 94L, 71L, 74L, 68L, 98L, 83L), w -> w + 2, worryReduceFunction, 3, 2, 7),
+                new Monkey(Arrays.asList(90L, 56L), w -> w * w, worryReduceFunction, 11, 3, 5)
         );
         int base = monkeys.stream().mapToInt(Monkey::getDivCheck).reduce((left, right) -> left * right).getAsInt();
-        monkeys.forEach(monkey -> monkey.setModBase(BigInteger.valueOf(base)));
+        monkeys.forEach(monkey -> monkey.setModBase(base));
         return monkeys;
     }
 
