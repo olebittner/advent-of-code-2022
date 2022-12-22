5a1d048796353582e2925af4b059c2fe35722986
Ole Bittner <dev@oleb.it>
2022-12-22 02:20:05 +0100

    Day 20: Grove Positioning System - Part 1

---
+++ src/day20/GrovePositioningSystem.java
@@ -0,0 +1,45 @@
+package day20;
+
+import util.Util;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class GrovePositioningSystem {
+
+    public static void main(String[] args) {
+        List<String> input = Util.readInput("day20/GrovePositioningSystem.txt");
+
+        List<Entry> in = new LinkedList<>();
+        for (int i = 0; i < input.size(); i++) {
+            in.add(new Entry(Integer.parseInt(input.get(i)), i));
+        }
+
+        for (int i = 0; i < in.size(); i++) {
+            final int finalI = i;
+            Entry entry = in.stream().filter(e -> e.getOriginalPosition() == finalI).findFirst().orElseThrow();
+            if (entry.getValue() == 0)
+                continue;
+
+            final int index = in.indexOf(entry);
+            in.remove(index);
+            int newIndex = index + entry.getValue();
+            int newIndexMod = Math.floorMod(newIndex, in.size());
+
+
+            if (newIndexMod == 0) {
+                newIndexMod = in.size();
+            }
+            in.add(newIndexMod, entry);
+        }
+
+        int zeroI = in.indexOf(in.stream().filter(e -> e.getValue() == 0).findFirst().orElseThrow());
+        int one = Math.floorMod(zeroI + 1000, in.size());
+        int two = Math.floorMod(zeroI + 2000, in.size());
+        int three = Math.floorMod(zeroI + 3000, in.size());
+
+        System.out.println(in.get(one).getValue() + in.get(two).getValue() + in.get(three).getValue());
+
+    }
+
+}
